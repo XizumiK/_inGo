c676dd423a92450b1d714062ca88e4f2f2b19e70
Kanagawa Izumi <izumikawaDT@outlook.com>
2024-02-26 11:38:38 +0800

    Update comments

--- datastructure/linkedList.go
+++ datastructure/linkedList.go
@@ -6,60 +6,77 @@ import (
 	"log"
 )
 
+// Node
+// the basic type of linked list
 type Node struct {
 	Data int
-	Next *Node
+	Next *Node // point to the next node
 }
 
+// NodeHead
+// the head of linked list, it doesn't contain data
 type NodeHead struct {
-	Next *Node
-	Nums int
+	Next *Node // point to the first node
+	Nums int   // the length of this linked list
 }
 
+// InsertAtBeginning
+// insert a node with provided data in the first place
 func (pointerToHead *NodeHead) InsertAtBeginning(data int) {
-	newNode := &Node{
+	newNode := &Node{ // Creat a new node with data
 		Data: data,
 	}
+	// insert newNode by exchanging the pointer
 	(*newNode).Next = (*pointerToHead).Next
 	(*pointerToHead).Next = newNode
-	pointerToHead.Nums++
+	pointerToHead.Nums++ // length + 1
 }
 
+// InsertAtEnd
+// insert a node with provided data in the end of the linked list
 func (pointerToHead *NodeHead) InsertAtEnd(data int) {
-	newNode := &Node{
+	newNode := &Node{ // Creat a new node with data
 		Data: data,
 	}
+	// find the end of the linked list
 	endNode := pointerToHead.Next
 	for i := 1; i < pointerToHead.Nums; i++ {
 		endNode = endNode.Next
 	}
+	// insert newNode by exchanging the pointer
 	newNode.Next = endNode.Next
 	endNode.Next = newNode
-	pointerToHead.Nums++
+	pointerToHead.Nums++ // length + 1
 }
 
+// Insert
+// insert data behind position, 0 - First; 1 - 2nd etc
 func (pointerToHead *NodeHead) Insert(data int, position int) {
-	if position > pointerToHead.Nums {
+	if position > pointerToHead.Nums { // if the position is out of range
 		err := errors.New("position is not exist in the linked list")
 		log.Println(err.Error())
 		return
-	} else if position == 0 {
+	} else if position == 0 { // InsertAtBeginning
 		pointerToHead.InsertAtBeginning(data)
 		return
 	}
 
-	newNode := &Node{
+	newNode := &Node{ // Creat a new node with data
 		Data: data,
 	}
+	// find the insert position of the linked list
 	theNode := pointerToHead.Next
 	for i := 1; i < position; i++ {
 		theNode = theNode.Next
 	}
+	// insert newNode by exchanging the pointer
 	newNode.Next = theNode.Next
 	theNode.Next = newNode
-	pointerToHead.Nums++
+	pointerToHead.Nums++ // length + 1
 }
 
+// Print
+// output all data and the length in the linked list
 func (pointerToHead *NodeHead) Print() {
 	temp := pointerToHead.Next
 	for temp != nil {
--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import "github.com/XizumiK/_inGo/datastructure"
 func main() {
 	var head datastructure.NodeHead // We create head which contains a pointer that points to a Node
 	(&head).InsertAtBeginning(0)
-	head.InsertAtBeginning(1) // Yes, you can use <head> to replace <(&head)>
+	head.InsertAtBeginning(1) // You can use <head> to replace <(&head)>
 	head.InsertAtBeginning(2)
-	head.Insert(9, 1) // But I hate that because it makes the harder to read
+	head.Insert(9, 1)
 	head.InsertAtEnd(8)
 	head.Print()
 }
